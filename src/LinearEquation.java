a3306e6418e962e3fe4e96ea7e93a9dcd5fb0bf8
BT_2S14_13 <email@email>
2024-10-01 11:01:49 -0400

    Added stuff lol

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -5,6 +5,7 @@ public class LinearEquation {
     private double y2;
     private String slope;
     private double yIntercept;
+    private double distanceBetweenPoints;
 
     public LinearEquation(double x1, double y1, double x2, double y2)
     {
@@ -14,12 +15,13 @@ public class LinearEquation {
         this.y2 = y2;
         slope = ((y2 - y1) + "/" + (x2 - x1));
         yIntercept = (y1 - (Double.parseDouble(slope) * x1));
+        distanceBetweenPoints = Math.sqrt(((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1)));
 
     }
     public String toString()
     {
 
-        return "First Pair: (" + x1 + "," + y1 + ") \n Second Pair: (" + x2 + "," + y2 + ") \n Slope of Line: " + slope + "\n Y intercept: " + yIntercept + "\n Distance between points: ";
+        return "First Pair: (" + x1 + "," + y1 + ") \n Second Pair: (" + x2 + "," + y2 + ") \n Slope of Line: " + slope + "\n Y intercept: " + yIntercept + "\n Distance between points: " + distanceBetweenPoints;
     }
 
 }
