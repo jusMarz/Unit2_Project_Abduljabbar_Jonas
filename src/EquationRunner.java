cdf544a35fe38794f486bbc5da362a5c445af891
BT_2S14_13 <email@email>
2024-10-02 10:54:16 -0400

    -Made the code take in user inputted coordinates with parenthesis using substring majiks -Fixed error in the toString method to display correct slope.

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 public class EquationRunner {
     public static void main(String[] args) {
 
-        LinearEquation s = new LinearEquation(-1,1,-2,-3);
+        Scanner userInput = new Scanner(System.in);
+
+
+        System.out.println("Enter your first Coordinate point: ");
+        String firstCoordinate = userInput.nextLine();
+        System.out.println("Enter your second Coordinate point: ");
+        String secondCoordinate = userInput.nextLine();
+        //(Double.parseDouble(firstCoordinate.substring(firstCoordinate.indexOf("(") + 1,firstCoordinate.indexOf(",")))) <- template
+        LinearEquation s = new LinearEquation(
+                (Integer.parseInt(firstCoordinate.substring(firstCoordinate.indexOf("(") + 1,firstCoordinate.indexOf(",")))),
+                (Integer.parseInt(firstCoordinate.substring(firstCoordinate.indexOf(",") + 1,firstCoordinate.indexOf(")")))),
+                (Integer.parseInt(secondCoordinate.substring(secondCoordinate.indexOf("(") + 1,secondCoordinate.indexOf(",")))),
+                (Integer.parseInt(secondCoordinate.substring(secondCoordinate.indexOf(",") + 1,secondCoordinate.indexOf(")")))));
         System.out.println(s);
         System.out.println("Enter in a third x value: ");
+        double x = Double.parseDouble(userInput.nextLine());
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,19 +1,19 @@
 public class LinearEquation {
-    private double x1;
-    private double y1;
-    private double x2;
-    private double y2;
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
     private double slope;
     private double yIntercept;
     private double distanceBetweenPoints;
 
-    public LinearEquation(double x1, double y1, double x2, double y2)
+    public LinearEquation(int x1, int y1, int x2, int y2)
     {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
-        slope = ((y2 - y1) /+ (x2 - x1));
+        slope = (((double) y2 - y1) /+ ((double) x2 - x1));
         yIntercept = (y1 - (slope) * x1);
         distanceBetweenPoints = ((Math.sqrt(((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1)))) + .005);
         distanceBetweenPoints -= distanceBetweenPoints % .01;
@@ -21,7 +21,7 @@ public class LinearEquation {
     public String toString()
     {
 
-        return "First Pair: (" + x1 + "," + y1 + ") \n Second Pair: (" + x2 + "," + y2 + ") \n Slope of Line: " + slope + "\n Y intercept: " + yIntercept + "\n Slope intercept form: " + (x2 -x1)+"/"+ (y2-y1)+ "x + "+ yIntercept + "\n Distance between points: " + distanceBetweenPoints;
+        return "First Pair: (" + x1 + "," + y1 + ") \n Second Pair: (" + x2 + "," + y2 + ") \n Slope of Line: " + slope + "\n Y intercept: " + yIntercept + "\n Slope intercept form: " + (y2 -y1)+"/"+ (x2-x1)+ "x + "+ yIntercept + "\n Distance between points: " + distanceBetweenPoints;
     }
 
 }
