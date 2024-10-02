59e07baa85c93298f6c41a23adfdcd885790b639
BT_2S14_13 <email@email>
2024-10-02 10:10:14 -0400

    -Changed String slope to double slope -Added rounding to nearest hundredth to distanceBetweenPoints -Added Slope intercept form to toString method

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,5 +1,8 @@
 public class EquationRunner {
     public static void main(String[] args) {
 
+        LinearEquation s = new LinearEquation(-1,1,-2,-3);
+        System.out.println(s);
+        System.out.println("Enter in a third x value: ");
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -3,7 +3,7 @@ public class LinearEquation {
     private double y1;
     private double x2;
     private double y2;
-    private String slope;
+    private double slope;
     private double yIntercept;
     private double distanceBetweenPoints;
 
@@ -13,15 +13,15 @@ public class LinearEquation {
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
-        slope = ((y2 - y1) + "/" + (x2 - x1));
-        yIntercept = (y1 - (Double.parseDouble(slope) * x1));
-        distanceBetweenPoints = Math.sqrt(((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1)));
-
+        slope = ((y2 - y1) /+ (x2 - x1));
+        yIntercept = (y1 - (slope) * x1);
+        distanceBetweenPoints = ((Math.sqrt(((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1)))) + .005);
+        distanceBetweenPoints -= distanceBetweenPoints % .01;
     }
     public String toString()
     {
 
-        return "First Pair: (" + x1 + "," + y1 + ") \n Second Pair: (" + x2 + "," + y2 + ") \n Slope of Line: " + slope + "\n Y intercept: " + yIntercept + "\n Distance between points: " + distanceBetweenPoints;
+        return "First Pair: (" + x1 + "," + y1 + ") \n Second Pair: (" + x2 + "," + y2 + ") \n Slope of Line: " + slope + "\n Y intercept: " + yIntercept + "\n Slope intercept form: " + (x2 -x1)+"/"+ (y2-y1)+ "x + "+ yIntercept + "\n Distance between points: " + distanceBetweenPoints;
     }
 
 }
